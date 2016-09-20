e07bd3c5edc29b5cb25d376a122bdf817913ad13
princvaliant <avolos@gmail.com>
2016-09-20 14:21:54 -0700

    Include more variables in scheduler for FAB reports

--- server/assembly.scheduler.js
+++ server/assembly.scheduler.js
@@ -29,8 +29,16 @@ Meteor.methods({
 var query = {
     parentCode: null,
     'value.tags': {$in: [
+        'nwLED|direct_view_baseline|ito_mesa_patterning',
+        'nwLED|direct_view_baseline|mesa_etch_pr_strip',
+        'nwLED|direct_view_baseline|lto_ald_deposition',
+        'nwLED|direct_view_baseline|lto_ald_pr_strip',
         'nwLED|direct_view_baseline|metal_patterning',
-        'nwLED|direct_view_baseline|mesa_etch_pr_strip'
+        'nwLED|direct_view_baseline|post_anneal',
+        'nwLED|direct_view_baseline|metal_liftoff',
+        'nwLED|direct_view_baseline|isolation_patterning',
+        'nwLED|direct_view_baseline|isolation_pr_strip',
+        'nwLED|direct_view_baseline|post_isolation_bond_pad_inspection'
     ]},
     'value.productCode': '100'
 };
@@ -39,8 +47,16 @@ var fields = {
     __id: '$_id',
     code: '$code',
     expId: '$value.experimentId',
+    itomesa: '$value.ito_mesa_patterning',
+    mesaetchpr: '$value.mesa_etch_pr_strip',
+    ltoalddep: '$value.lto_ald_deposition',
+    ltoaldpr: '$value.lto_ald_pr_strip',
     metpat: '$value.metal_patterning',
-    mesaetchpr: '$value.mesa_etch_pr_strip'
+    postann: '$value.post_anneal',
+    metlift: '$value.metal_liftoff',
+    isopat: '$value.isolation_patterning',
+    isopr: '$value.isolation_pr_strip',
+    postinsp: '$value.post_isolation_bond_pad_inspection'
 };
 
 function getPipeline() {
