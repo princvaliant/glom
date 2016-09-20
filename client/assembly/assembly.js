349257b835d67b1b6175e38ae6f06fa8a82f4421
princvaliant <avolos@gmail.com>
2016-09-20 15:35:44 -0700

    Add additional charts

--- client/assembly/assembly.js
+++ client/assembly/assembly.js
@@ -12,13 +12,17 @@ Template.assembly.helpers({
     }
 });
 
+$('.nav-pills a').click(function (e) {
+    console.log(e);
+});
+
 var chartDefs = [{
     id: 'chart1',
-    title: 'Pattern depth',
-    ytitles: ['Depth 1', 'Depth 3', 'Depth 6'],
-    ymax: 8,
-    ymin: 5,
-    yfields: ['metpat.pattern_depth1', 'metpat.pattern_depth3', 'metpat.pattern_depth6']
+    title: 'ITO Mesa patterning',
+    // ymax: 3,
+    // ymin: 0,
+    ytitles: ['Resist thickness'],
+    yfields: ['itomesa.resist_thickness']
 }, {
     id: 'chart2',
     title: 'Mesa height',
@@ -26,6 +30,48 @@ var chartDefs = [{
     ymin: 0,
     ytitles: ['Mesa height 1', 'Mesa height 3', 'Mesa height 6'],
     yfields: ['mesaetchpr.mesa_height_1', 'mesaetchpr.mesa_height_3', 'mesaetchpr.mesa_height_6']
+}, {
+    id: 'chart3',
+    title: 'ITO pad size',
+    // ymax: 3,
+    // ymin: 0,
+    ytitles: ['ITO min pad size', 'ITO max pad size'],
+    yfields: ['mesaetchpr.ito_pad_size_min', 'mesaetchpr.ito_pad_size_max']
+}, {
+    id: 'chart4',
+    title: 'ITO ALD PR strip',
+    // ymax: 3,
+    // ymin: 0,
+    ytitles: ['LTO size opening', 'LTO opening misalignment'],
+    yfields: ['ltoaldpr.lto_size_opening', 'ltoaldpr.lto_opening_missalignment']
+}, {
+    id: 'chart5',
+    title: 'LTO ALD deposition',
+    // ymax: 8,
+    // ymin: 5,
+    ytitles: ['LTO witness thickness'],
+    yfields: ['ltoalddep.lto_witness_thickness']
+}, {
+    id: 'chart6',
+    title: 'Post anneal',
+    // ymax: 8,
+    // ymin: 5,
+    ytitles: ['Witness sheet resistance', 'Transmission'],
+    yfields: ['postann.witness_sheet_resistance', 'postann.transmission']
+}, {
+    id: 'chart7',
+    title: 'Metal patterning depth',
+    ytitles: ['Depth 1', 'Depth 3', 'Depth 6'],
+    ymax: 8,
+    ymin: 5,
+    yfields: ['metpat.pattern_depth1', 'metpat.pattern_depth3', 'metpat.pattern_depth6']
+}, {
+    id: 'chart8',
+    title: 'Metal base pressure',
+    ytitles: ['Base pressure'],
+    // ymax: 8,
+    // ymin: 5,
+    yfields: ['metpat.base_pressure']
 }];
 
 function assemblyProcessing (datalist) {
@@ -63,24 +109,25 @@ function assemblyProcessing (datalist) {
             data: []
         };
 
-        for (var i = 0; i < 3; i++) {
-            var series = {
-                type: 'scatter',
-                markerSize: 5,
-                toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong> Wafer</strong> {label} <br/><strong> Value</strong></span> {y}<br/>",
-                name: chartDef.ytitles[i],
-                showInLegend: true,
-                dataPoints: []
-            };
-
-            var fields = chartDef.yfields[i].split('.');
-            _.each(datalist, function (data) {
-                series.dataPoints.push({
-                    y: data[fields[0]] ? data[fields[0]][fields[1]] : null,
-                    label: data.expId + ' ' + data.code
+        for (var i = 0; i < 10; i++) {
+            if (chartDef.yfields[i]) {
+                var series = {
+                    type: 'scatter',
+                    markerSize: 5,
+                    toolTipContent: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span><br/><strong> Wafer</strong> {label} <br/><strong> Value</strong></span> {y}<br/>",
+                    name: chartDef.ytitles[i],
+                    showInLegend: true,
+                    dataPoints: []
+                };
+                var fields = chartDef.yfields[i].split('.');
+                _.each(datalist, function (data) {
+                    series.dataPoints.push({
+                        y: data[fields[0]] ? data[fields[0]][fields[1]] : null,
+                        label: data.expId + ' ' + data.code
+                    });
                 });
-            });
-            chart.data.push(series);
+                chart.data.push(series);
+            }
         }
 
 
@@ -96,101 +143,4 @@ function assemblyProcessing (datalist) {
         };
     });
     Session.set('tasksLoading', false);
-
-    //
-    // var group= _.groupBy(datalist, function(o){
-    //   return o.expId || o.code;
-    // });
-    // var pass = [];
-    // for (var key in wafergroup) {
-    //   var list = wafergroup[key];
-    //   var obj = {label: key, pbp: 0, pbpt: 0, pi: 0, pass: 0};
-    //   _.each(list, function(o) {
-    //     if (o.pbp) {
-    //       obj.pbp += 1;
-    //     } else
-    //     if (o.pbpt) {
-    //       obj.pbpt += 1;
-    //     } else
-    //     if (o.pi) {
-    //       obj.pi += 1;
-    //     } else
-    //     if (!o.pbp && !o.pbpt && !o.pi) {
-    //       obj.pass += 1;
-    //     }
-    //   });
-    //   pass.push(obj);
-    // }
-    // constructChart(_.sortBy(pass, 'label'));
-}
-
-
-function constructChart (list) {
-    var chart = new CanvasJS.Chart("chartCY1",
-        {
-            title: {
-                text: "Coupon yield"
-            },
-            axisY: {
-                title: "Coupons",
-                titleFontSize: 14,
-                labelFontSize: 11,
-                gridThickness: 1
-            },
-            axisX: {
-                title: "Exp# - Wafer#",
-                titleFontSize: 14,
-                labelFontSize: 11,
-                labelAngle: -35,
-                interval: 1
-            },
-            animationEnabled: true,
-            data: [
-                {
-                    type: "stackedColumn",
-                    toolTipContent: "{label}<br/><span style='\"'color: {color};'\"'><strong>{name}</strong></span>: {y}",
-                    name: "PASS",
-                    showInLegend: "true",
-                    dataPoints: pick(list, 'pass')
-                }, {
-                    type: "stackedColumn",
-                    toolTipContent: "{label}<br/><span style='\"'color: {color};'\"'><strong>{name}</strong></span>: {y}",
-                    name: "Post bond pad",
-                    showInLegend: "true",
-                    dataPoints: pick(list, 'pbp')
-                }, {
-                    type: "stackedColumn",
-                    toolTipContent: "{label}<br/><span style='\"'color: {color};'\"'><strong>{name}</strong></span>: {y}",
-                    name: "Post bond pad test",
-                    showInLegend: "true",
-                    dataPoints: pick(list, 'pbpt')
-                }, {
-                    type: "stackedColumn",
-                    toolTipContent: "{label}<br/><span style='\"'color: {color};'\"'><strong>{name}</strong></span>: {y}",
-                    name: "Post isolation",
-                    showInLegend: "true",
-                    dataPoints: pick(list, 'pi')
-                }],
-            legend: {
-                cursor: "pointer",
-                itemclick: function (e) {
-                    if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-                        e.dataSeries.visible = false;
-                    }
-                    else {
-                        e.dataSeries.visible = true;
-                    }
-                    chart.render();
-                }
-            }
-        });
-
-    chart.render();
-}
-
-function pick (pass, field) {
-    var ret = _.map(pass, function (obj, key) {
-        return {label: obj.label, y: obj[field]};
-    });
-    return ret;
 }
